0930eaada28d5e6cc9f570b4f0e1cd432d6644c0
Marcelo Tena <marcelotena@fastandyours.com>
2016-10-09 11:12:20 +0200

    Setting up theme: Base stylesheets based on underscores.me.

---
+++ functions.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * WCSantander16 Demo functions and definitions.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WCSantander16_Demo
+ */
+
+if ( ! function_exists( 'wp_ng_spa_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function wp_ng_spa_setup() {
+	/*
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 * If you're building a theme based on components, use a find and replace
+	 * to change 'wp_ng_spa' to the name of your theme in all the template files.
+	 */
+	load_theme_textdomain( 'wp_ng_spa', get_template_directory() . '/languages' );
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 */
+	add_theme_support( 'post-thumbnails' );
+
+	add_image_size( 'wp_ng_spa-featured-image', 640, 9999 );
+
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus( array(
+		'menu-1' => esc_html__( 'Top', 'wp_ng_spa' ),
+		) );
+
+	/**
+	 * Add support for core custom logo.
+	 */
+	add_theme_support( 'custom-logo', array(
+		'height'      => 200,
+		'width'       => 200,
+		'flex-width'  => true,
+		'flex-height' => true,
+	) );
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'search-form',
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'caption',
+	) );
+
+	// Set up the WordPress core custom background feature.
+	add_theme_support( 'custom-background', apply_filters( 'wp_ng_spa_custom_background_args', array(
+		'default-color' => 'ffffff',
+		'default-image' => '',
+	) ) );
+}
+endif;
+add_action( 'after_setup_theme', 'wp_ng_spa_setup' );
+
+
+
+/**
+ * Enqueue scripts and styles.
+ */
+function wp_ng_spa_scripts() {
+	wp_enqueue_style( 'wp_ng_spa-style', get_stylesheet_uri() );
+
+	wp_enqueue_script( 'wp_ng_spa-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20151215', true );
+
+	wp_enqueue_script( 'wp_ng_spa-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'wp_ng_spa_scripts' );
