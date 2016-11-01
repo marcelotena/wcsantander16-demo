ba2d507e4d03f70c658a9376ef8109c7605e54d5
Marcelo Tena <marcelotena@fastandyours.com>
2016-11-01 16:17:45 +0100

    Viewport meta fixed.

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
     <html <?php language_attributes(); ?> ng-app="app">
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>" />
-        <meta name="viewport" content="width=device-width" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title><?php
 	/*
 	 * Print the <title> tag based on what is being viewed.
